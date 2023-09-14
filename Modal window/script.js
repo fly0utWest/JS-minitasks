cf3881989d694719596e09a42c2958d855404001
Nik <shealtov@gmail.com>
2023-09-14 13:53:05 +0300

    Modal window task code refactored.

--- Modal window/script.js
+++ Modal window/script.js
@@ -5,19 +5,18 @@ const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.close-modal');
 const showModal = document.querySelectorAll('.show-modal');
 
+const hideModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < showModal.length; i++) {
   showModal[i].addEventListener('click', () => {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
   });
 
-  overlay.addEventListener('click', () => {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  });
+  overlay.addEventListener('click', hideModal);
 
-  closeModal.addEventListener('click', () => {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  });
+  closeModal.addEventListener('click', hideModal);
 }
